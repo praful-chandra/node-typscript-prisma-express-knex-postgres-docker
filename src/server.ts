878af08e0af6b7cae619d2c7afd0085e8843f272
praful chandra <prafulchandra@prafuls-MacBook-Pro.local>
2021-10-06 23:01:58 +0530

    chore: added typescript support

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express,{Application,Request,Response} from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Application = express();
+
+const PORT: number = Number.parseInt(process.env.PORT || '8000');
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+app.get('/', (req: Request, res:Response) => {
+    res.json({message : "reached the home endpoint of oauth2 server"});
+});
