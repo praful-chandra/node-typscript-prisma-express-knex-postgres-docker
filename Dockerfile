30e4218d16056fd19550231f168fa3c75d1966b6
praful chandra <prafulchandra@prafuls-MacBook-Pro.local>
2021-10-07 16:21:23 +0530

    chore: added prisma

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ COPY package-lock.json ./
 
 RUN npm install
 
+COPY prisma/schema.prisma ./prisma/
+RUN npx prisma generate
+
 COPY . .
 
 RUN npm run build
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-import express,{Application,Request,Response} from 'express';
+import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
 dotenv.config();
 
 const app: Application = express();
@@ -7,9 +11,9 @@ const app: Application = express();
 const PORT: number = Number.parseInt(process.env.PORT || '8000');
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
 
-app.get('/', (req: Request, res:Response) => {
-    res.json({message : "reached the home endpoint of oauth2 server"});
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'reached the home endpoint of oauth2 server' });
 });
